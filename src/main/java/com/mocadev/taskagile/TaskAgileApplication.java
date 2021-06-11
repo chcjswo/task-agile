2544802f293ff65c361c5c7c5a08f7b17fd15511
tony <chcjswo@gmail.com>
2021-06-11 22:51:55 +0900

    Initial commit

---
+++ src/main/java/com/mocadev/taskagile/TaskAgileApplication.java
@@ -0,0 +1,13 @@
+package com.mocadev.taskagile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskAgileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskAgileApplication.class, args);
+	}
+
+}
