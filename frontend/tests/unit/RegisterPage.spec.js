d2a425b46cb0b4576f673a1b6661ff00a06ae4d8
tony <chcjswo@gmail.com>
2021-06-14 23:31:30 +0900

    test: add register test code

--- frontend/tests/unit/RegisterPage.spec.js
+++ frontend/tests/unit/RegisterPage.spec.js
@@ -1,5 +1,12 @@
-import {mount} from '@vue/test-utils';
+import {createLocalVue, mount} from '@vue/test-utils';
 import RegisterPage from "@/views/RegisterPage";
+import VueRouter from "vue-router";
+
+const localVue = createLocalVue();
+localVue.use(VueRouter);
+const router = new VueRouter();
+
+jest.mock('@/service/registration');
 
 describe('RegisterPage.vue', () => {
   let wrapper;
@@ -9,16 +16,25 @@ describe('RegisterPage.vue', () => {
   let buttonSubmit;
 
   beforeEach(() => {
-    wrapper = mount(RegisterPage);
+    wrapper = mount(RegisterPage, {
+      localVue,
+      router
+    });
     fieldUsername = wrapper.find('#username');
     filedEmailAddress = wrapper.find('#emailAddress');
     fieldPassword = wrapper.find('#password');
     buttonSubmit = wrapper.find('form button[type="submit"]');
   });
 
+  afterAll(() => {
+    jest.restoreAllMocks();
+  });
+
   it('should render correct contents', () => {
-    expect(wrapper.find('.logo').attributes().src).toEqual('/static/images/logo.png');
-    expect(wrapper.find('.tagline').text()).toEqual('Open source task management tool');
+    expect(wrapper.find('.logo').attributes().src).toEqual(
+      '/static/images/logo.png');
+    expect(wrapper.find('.tagline').text()).toEqual(
+      'Open source task management tool');
     expect(fieldUsername.element.value).toEqual('');
     expect(filedEmailAddress.element.value).toEqual('');
     expect(fieldPassword.element.value).toEqual('');
@@ -49,6 +65,27 @@ describe('RegisterPage.vue', () => {
     wrapper.setMethods({submitForm: stub});
     buttonSubmit.trigger('submit');
     expect(stub).toBeCalled();
+  });
+
+  it('should register when it is a new user', () => {
+    const stub = jest.fn();
+    wrapper.vm.$router.push = stub;
+    wrapper.vm.form.username = 'test';
+    wrapper.vm.form.emailAddress = 'test@test.com';
+    wrapper.vm.form.password = 'test';
+    wrapper.vm.submitForm();
+    wrapper.vm.$nextTick(() => {
+      expect(stub).toHaveBeenCalledWith({name: 'LoginPage'});
+    });
+  });
+
+  it('should fail when it is not a new user', () => {
+    wrapper.vm.form.emailAddress = 'chcjswo@test.com';
+    expect(wrapper.find('.failed').isVisible()).toBe(false);
+    wrapper.vm.submitForm();
+    wrapper.vm.$nextTick(() => {
+      expect(wrapper.find('.failed').isVisible()).toBe(true);
+    });
   });
 
 });
